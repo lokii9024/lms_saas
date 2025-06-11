eebf12e1599fa838877a09599dacc92c904c6e70
lokesh vaishnav <vishalvaishnav477@gmail.com>
2025-06-11 13:10:04 +0530

    implement home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
 import React from 'react'
 import {Button} from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import { recentSessions } from '@/constants'
+import Cta from '@/components/CTA'
+
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome!, to my saas application</h1>
-      <p className='text-lg mt-4'>This is a simple SaaS application built with Next.js and Tailwind CSS.</p>
-      <Button> Let's get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={30}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={45}
+          color="#FFDA6E"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <Cta />
+      </section>
+    </main>
   )
 }
 
